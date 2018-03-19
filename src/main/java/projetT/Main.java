328bbd0b90bd74c878bacea12b47899478f2c8f3
samuel beslot <yuckon49@gmail.com>
2018-03-19 13:33:35 +0100

    Envoit de la modification du Main

--- src/main/java/projetT/Main.java
+++ src/main/java/projetT/Main.java
@@ -1,5 +1,6 @@
 package projetT;
 
+import com.mysql.cj.jdbc.Driver;
 import fr.imie.chat.specification.WebSocketServer;
 import fr.imie.chat.specification.listeners.CloseWebSocketListener;
 import fr.imie.chat.specification.listeners.MessageWebSocketListener;
@@ -7,37 +8,27 @@ import fr.imie.chat.specification.listeners.OpenWebSocketListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import static java.sql.DriverManager.println;
 
 public class Main{
-public static void main(String [] args) throws IOException {
-    new Main();
-
+public static void main(String [] args) throws IOException, SQLException {
+        new Main();
     }
 
-    private Main() throws IOException {
+    private Main() throws IOException, SQLException {
 
+        DriverManager.registerDriver(new Driver());
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/projett?serverTimezone=UTC", "root", "");
         WebSocketServer<String> webSocketServer = WebSocketServer.get("localhost", 8080, String.class);
-        webSocketServer.addListener(new OpenWebSocketListener<String>(){
-            @Override
-            public void onOpen(String idSession){
-                println(" Ouvert sur " + idSession);
-            }
-        });
+        webSocketServer.addListener((OpenWebSocketListener<String>) idSession -> println(" Ouvert sur " + idSession));
 
-        webSocketServer.addListener(new CloseWebSocketListener<String>() {
-            @Override
-            public void onClose(String idSession) {
-                println(" Fermer sur " + idSession);
-            }
-        });
+        webSocketServer.addListener((CloseWebSocketListener<String>) idSession -> println(" Fermer sur " + idSession));
 
-        webSocketServer.addListener(new MessageWebSocketListener<String>() {
-            @Override
-            public void onMessage(String message, String idSession) {
-                println(" Votre message: " + message + " de " + idSession);
-            }
-        });
+        webSocketServer.addListener((MessageWebSocketListener<String>) (message, idSession) -> println(message + " de " + idSession));
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println(" Si tu veux arrêter le serveur appuie sur une touche ;) ");
         reader.readLine();
